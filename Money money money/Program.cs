2a08859a453a64d2f1cecadf8151b689842d396c
Tim-Audenaert <tim.audenaert@hotmail.be>
2021-03-23 08:42:53 +0100

    Add project files.

---
+++ Money money money/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_money_money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankRekening bank = new BankRekening();
+            bank.VoegGeldToe(10000);
+
+            SpaarRekening spaar = new SpaarRekening();
+            spaar.VoegGeldToe(10000);
+
+            ProRekening pro = new ProRekening();
+            pro.VoegGeldToe(10000);
+
+
+            Console.WriteLine("Bankrekening Saldo: " + bank.Saldo + " - Rente: " + bank.BerekenRente());
+            Console.WriteLine("Spaarrekening Saldo: " + spaar.Saldo + " - Rente: " + spaar.BerekenRente());
+            Console.WriteLine("Prorekening Saldo: " + pro.Saldo + " - Rente: " + pro.BerekenRente());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Money money money/Rekening.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_money_money
+{
+    abstract class Rekening
+    {
+        public string Naam { get; set; }
+        public double Saldo { get; private set; }
+        public double Rente { get; set; }
+        public void VoegGeldToe(double bedrag)
+        {
+            Saldo += bedrag;
+        }
+        public void HaalGeldAf(double bedrag)
+        {
+            Saldo -= bedrag;
+        }
+        public abstract double BerekenRente();
+    }
+
+    class BankRekening : Rekening
+    {
+        public override double BerekenRente()
+        {
+            if (Saldo > 100)
+            {
+                Rente = Saldo * 0.05;
+            }
+            return Rente;
+        }
+    }
+
+    class SpaarRekening : Rekening
+    {
+        public override double BerekenRente()
+        {
+            Rente = Saldo * 0.02;
+            return Rente;
+        }
+    }
+
+    class ProRekening : SpaarRekening
+    {
+        public override double BerekenRente()
+        {
+            double temp = Saldo;
+            double extraRente = 0;
+            while (temp >= 1000)
+            {
+                temp -= 1000;
+                extraRente += 10;
+            }
+
+            return base.BerekenRente() + extraRente;
+        }
+    }
+}
